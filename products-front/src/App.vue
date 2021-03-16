bd75685aad265caf1841abb7ad8dda732fee4f4e
Gustavo Riquena <gustavo_riquena1@outlook.com>
2021-03-16 08:52:10 -0300

    removing helloWorld component and listing data from api

--- products-front/src/App.vue
+++ products-front/src/App.vue
@@ -16,25 +16,25 @@
           <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
       </form>
       <table>
-          <thead>
-              <tr>
-                <th>NOME</th>
-                <th>QTD</th>
-                <th>VALOR</th>
-                <th>OPÇÕES</th>
-            </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
-            <td>
-              <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
-              <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
-            </td>
-          </tr>
-        </tbody>
+            <thead>
+                <tr>
+                    <th>NOME</th>
+                    <th>QTD</th>
+                    <th>VALOR</th>
+                    <th>OPÇÕES</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="produto of produtos" :key="produto.id" >
+                    <td>{{ produto.nome }}</td>
+                    <td>{{ produto.quantidade }}</td>
+                    <td>{{ produto.valor }}</td>
+                    <td>
+                        <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+                        <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+                    </td>
+                </tr>
+            </tbody>
       </table>
     </div>
   </div>
@@ -42,12 +42,20 @@
 
 <script>
 
-import Produto from './services/produtos';
+import Produto from '../services/produtos.js';
 
 export default {
+
+    data(){
+        return{
+            produtos: []
+        }
+    },
+
     mounted(){
         Produto.listar().then(resposta => {
-            console.log(resposta);
+            console.log(resposta.data);
+            this.produtos = resposta.data
         })
     }
 }
